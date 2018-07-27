c921ecd0f047a343f213a1f87eac03ad17f882d9
yupc <357001799@qq.com>
2018-07-27 10:46:30 +0800

    modified put_to_sotre

--- chapter7.py
+++ chapter7.py
@@ -15,10 +15,21 @@ def get_coach_data(filename):
 
 def put_to_stroe(files_list):
     all_athletes = {}
-
+    for each_file in files_list:
+        ath  = get_coach_data(each_file)
+        all_athletes[ath.name]  = ath
+    try:
+        with open('athletes.pickle', 'wb') as athf:
+            pickle.dump(all_athletes,athf)
+    except IOError as ioerr:
+        print('File ERROR(put_to_store):' + str(ioerr))
     return all_athletes
 
 def get_from_store():
     all_athletes = {}
-
+    try:
+        with open('athletes.pickle', 'rb') as athf:
+            all_athletes = pickle.load(athf)
+    except IOError as ioerr:
+        print('File Error(get_from_store):' + str(ioerr))
     return all_athletes
