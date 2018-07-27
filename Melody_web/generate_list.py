ce3a64f48d35b6592e9a2aef417a13a9ca8f9ba2
yupc <357001799@qq.com>
2018-07-27 16:10:37 +0800

    pool quest

--- Melody_web/generate_list.py
+++ Melody_web/generate_list.py
@@ -1,4 +1,10 @@
 import glob
-import athletemodel
-import yate
+from Melody_web import athletemodel
+from Melody_web import yate
+
 data_files = glob.glob("data/*.txt")
+athletes = athletemodel.put_to_stroe(data_files)
+
+
+
+print(yate.include_footer({"Home": "/index.html"}))
